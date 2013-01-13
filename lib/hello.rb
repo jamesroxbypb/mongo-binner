7d23564b132816c2c5b186d5f2e84e3cada11862
Phil James-Roxby <phil.jamesroxby@gmail.com>
2013-01-13 09:23:14 -0700

    Getting there

--- lib/hello.rb
+++ lib/hello.rb
@@ -6,12 +6,14 @@ include Mongo
 
 @config = YAML.load_file "../config/config.yaml"
 @client = MongoClient.new(@config["mongo-host"], 27017)
-@db     = @client["mongo-db"]
+@db     = @client[@config["mongo-db"]]
 
 collection = "items"
 in_need = @db[collection].find({"bin_analysis" => {"$ne" => true}})
 in_need.each {|n|
-   puts n
-}
+  t = n["_id"].generation_time 
+  puts n
 binding.pry
+}
+
 
