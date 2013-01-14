eba5238198ac8a0b1379922023e93f9b58e9e7f2
Phil James-Roxby <phil.jamesroxby@gmail.com>
2013-01-14 09:52:34 -0700

    Kind of works

--- config.ru
+++ config.ru
@@ -1,3 +1,5 @@
-
+require "yaml"
 require "./lib/webapp.rb"
+
+@config = YAML.load_file "./config/config.yaml"
 run MongoBinner
--- lib/webapp.rb
+++ lib/webapp.rb
@@ -1,15 +1,50 @@
 require "sinatra"
 require "json"
+require "mongo"
 require "sinatra/reloader"
 
 class MongoBinner < Sinatra::Base
+
 get "/hi" do
   body "Hello world"
   status 200
 end
 
-get "/graph" do
+get "/:collection/graph" do
+    # There is way too much going on here, but ...
+	@config = YAML.load_file "./config/config.yaml"
+	@client = Mongo::MongoClient.new(@config["mongo-host"], 27017)
+	@db     = @client[@config["mongo-db"]]
+
+	collection = params[:collection]
+	analysis = "#{collection}.analysis"
+	resolution = 15*60
+
+	in_need = @db[collection].find({"bin_analysis" => {"$ne" => true}})
+	in_need.each {|n|
+	  t = n["_id"].generation_time.to_i
+	  div = t / resolution
+	  rounded = div * resolution
+	  time_bin = Time.at(rounded)
+	  @db[analysis].update({"bin" => time_bin},{"$inc" => {"count" => 1}}, {:upsert => true})
+	  n["bin_analysis"] = true
+	  @db[collection].save(n)
+	}
+	to_graph = @db[analysis].find()
 	@wibble = 11
+	@item = collection
+	@rows = []
+	to_graph.each {|item|
+		next_entry = Hash.new()
+		next_entry["year"] = item["bin"].year
+		next_entry["month"] = item["bin"].month-1 # Ah JavaScript
+		next_entry["day"] = item["bin"].day
+		next_entry["hour"] = item["bin"].hour
+		next_entry["min"] = item["bin"].min
+		next_entry["sec"] = item["bin"].sec
+		next_entry["count"] = item["count"]
+		@rows.push(next_entry)
+	}
 	erb :graph
 end
 
