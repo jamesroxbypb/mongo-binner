906386ebb22977b38f96e525086bc24dc2e81cec
Phil James-Roxby <phil.jamesroxby@gmail.com>
2013-01-15 19:45:15 -0700

    Added item get

--- lib/things.rb
+++ lib/things.rb
@@ -2,6 +2,7 @@ require "sinatra"
 require "json"
 require "mongo"
 require "sinatra/reloader"
+require "yaml"
 
 class Things < Sinatra::Base
 
@@ -15,24 +16,45 @@ get "/hi" do
 end
 
 get "/:tenant/:collection" do
-	limit = 20
-	offset = 0
-	limit  = Integer(params["limit"]) if params.has_key?("limit")
-	offset = Integer(params["offset"]) if params.has_key?("offset")
+    limit = 20
+    offset = 0
+    limit  = Integer(params["limit"]) if params.has_key?("limit")
+    offset = Integer(params["offset"]) if params.has_key?("offset")
 
-	@config = YAML.load_file "./config/config.yaml"
-	@client = Mongo::MongoClient.new(@config["mongo-host"], 27017)
-	@db     = @client[@config["mongo-db"]]
+    @config = YAML.load_file "./config/config.yaml"
+    @client = Mongo::MongoClient.new(@config["mongo-host"], 27017)
+    @db     = @client[@config["mongo-db"]]
 
-	collection = "#{params[:tenant]}.#{params[:collection]}"
-	results = @db[collection].find({},{:limit => limit, :skip => offset}).to_a
-	results.each {|r|
-		r["id"] = r["_id"].to_s
-		r.delete "_id"
-	}
-	body results.to_json
-	status 200
+    collection = "#{params[:tenant]}.#{params[:collection]}"
+    results = @db[collection].find({},{:limit => limit, :skip => offset}).to_a
+    results.each {|r|
+      r["id"] = r["_id"].to_s
+      r.delete "_id"
+    }
+    body results.to_json
+    status 200
 end
+
+get "/:tenant/:collection/:id" do
+
+    @config = YAML.load_file "./config/config.yaml"
+    @client = Mongo::MongoClient.new(@config["mongo-host"], 27017)
+    @db     = @client[@config["mongo-db"]]
+
+    collection = "#{params[:tenant]}.#{params[:collection]}"
+    result = @db[collection].find_one({"_id" => BSON::ObjectId(params["id"])})
+    if (result)
+      result["id"] = result["_id"].to_s
+      result.delete "_id"
+      
+      body result.to_json
+      status 200
+    else
+      status 404
+    end
+end
+
+
 
 post "/:tenant/:collection" do
 	blob = JSON.parse request.body.read
